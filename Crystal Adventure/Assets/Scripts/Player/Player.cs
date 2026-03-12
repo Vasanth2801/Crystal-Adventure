51f1452208d5968b905d41371c870516a234f512
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-12 12:16:24 +0530

    Attack Animation and triggering for the player

--- Crystal Adventure/Assets/Scripts/Player/Player.cs
+++ Crystal Adventure/Assets/Scripts/Player/Player.cs
@@ -65,6 +65,7 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.L))
         {
+            animator.SetTrigger("Attack");
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
             foreach(Collider2D hit in hitEnemies)
             {
